fa068c375d00dbcdee9d815444180d28b19a8302
huhonmcmc <huhonmcmc@gmail.com>
2025-12-30 14:40:31 +0900

    refactor :: jwt 암호화 버전 오류로 인한 수정

--- app/core/security/password.py
+++ app/core/security/password.py
@@ -1,7 +1,9 @@
 from passlib.context import CryptContext
 
 # 비밀번호 해싱 컨텍스트
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+# bcrypt_sha256: bcrypt의 72바이트 제한을 우회하기 위해 SHA256 사전 해싱 사용
+# Python 3.12 + passlib 1.7.4 + bcrypt 3.2.2 호환성 보장
+pwd_context = CryptContext(schemes=["bcrypt_sha256"], deprecated="auto")
 
 
 def hash_password(password: str) -> str:
